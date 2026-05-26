3b4cc20fe3beb04649660d3fa4e717a4264f233c
Shivaji Pandurang Jadhav <shiva.281191@gmail.com>
2026-05-26 09:16:35 -0400

    feat:new chnages favourite racipe code

--- src/redux/favoritesSlice.js
+++ src/redux/favoritesSlice.js
@@ -8,7 +8,22 @@ const favoritesSlice = createSlice({
   name: "favorites",
   initialState,
   reducers: {
-   
+   toggleFavorite: (state, action) => {
+      const recipeToToggle = action.payload;
+      
+      // Check if the recipe already exists in favorites by comparing idFood
+      const existingIndex = state.favoriterecipes.findIndex(
+        (recipe) => recipe.idFood === recipeToToggle.idFood
+      );
+
+      if (existingIndex >= 0) {
+        // If it exists, remove the item from the list
+        state.favoriterecipes.splice(existingIndex, 1);
+      } else {
+        // If it doesn't exist, add the item to the favorites list
+        state.favoriterecipes.push(recipeToToggle);
+      }
+    },
   },
 });
 
