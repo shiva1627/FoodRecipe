8a8c50524752918d9050f8456174c9bbddb42d9d
Shivaji Pandurang Jadhav <shiva.281191@gmail.com>
2026-05-27 02:44:33 -0400

    new chnages for run on device

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createStackNavigator } from "@react-navigation/stack";
 import HomeScreen from "../screens/HomeScreen";
 import WelcomeScreen from "../screens/WelcomeScreen";
 import MyRecipeScreen from "../screens/MyRecipeScreen";
